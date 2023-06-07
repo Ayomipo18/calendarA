896044b31a0b2b168b16c7c2b16e20d4e19f80c8
Ayomipo Solaja <ayomiposolaja@gmail.com>
2023-06-07 08:10:43 +0100

    added pagination, filtering, searching on Meeting

--- src/controllers/meeting.controller.ts
+++ src/controllers/meeting.controller.ts
@@ -1,8 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import IServiceManager from '../services/interfaces/iserviceManager';
 import { injectable, inject } from "inversify";
-import TYPES from "../types";
-import { AuthDTO } from '../dtos/AuthDTO';
+import TYPES from "../di/types";
+import { MeetingParameter } from '../helpers/ResourceParameter';
+import { ValidatedRequest } from 'express-joi-validation'
+import { meetingResourceSchema } from '../validators/meeting.validator';
+import { resourceParameter } from '../helpers/constants';
+import { LoggedInUser } from '../dtos/UserDTO';
 
 @injectable()
 export default class MeetingController {
@@ -12,10 +16,14 @@ export default class MeetingController {
         this._service = service;
     }
 
-    public async getAllMeetings(req: Request, res: Response, next: NextFunction): Promise<any>{
+    public async getAllMeetings(req: ValidatedRequest<meetingResourceSchema>, res: Response, next: NextFunction): Promise<any>{
         try {
-            const data = await this._service.Meeting.getAllMeetings();
-            return res.status(data.status).json({data: data.data, message: data.message});
+            const meetingParameters : MeetingParameter = req.query;
+            meetingParameters.pageNumber ??= resourceParameter.pageNumber;
+            meetingParameters.pageSize ??= resourceParameter.pageSize;
+            const loggedInUser: LoggedInUser = req.user;
+            const data = await this._service.Meeting.getAllMeetings(meetingParameters, loggedInUser);
+            return res.status(data.status).json({data: data.data, meta: data.meta, message: data.message});
         } catch(error) {
             next(error);
         }
--- src/services/meeting.service.ts
+++ src/services/meeting.service.ts
@@ -1,13 +1,14 @@
 import IRepositoryManager from '../repositories/interfaces/irepositoryManager';
 import IMeetingService from './interfaces/imeeting.service';
 import { injectable, inject } from "inversify";
-import TYPES from '../types';
-import SuccessResponse from '../helpers/SuccessResponse';
+import TYPES from '../di/types';
+import { PagedResponse, Meta } from '../helpers/PagedResponse';
 import { mapper } from '../mappings/mapper';
 import { Meeting } from '../models/interfaces/imeeting.model';
 import { MeetingResponse } from '../dtos/MeetingDTO';
-import { Event } from '../models/interfaces/ievent.model';
-import { EventResponse } from '../dtos/EventDTO';
+import { MeetingParameter } from '../helpers/ResourceParameter';
+import { LoggedInUser } from '../dtos/UserDTO';
+import { StatusCodes } from 'http-status-codes';
 
 @injectable()
 export default class MeetingService implements IMeetingService {
@@ -17,11 +18,63 @@ export default class MeetingService implements IMeetingService {
         this._repository = repository;
     }
 
-    public async getAllMeetings(): Promise<SuccessResponse<Array<MeetingResponse>>> {
-        const meetings = await this._repository.Meeting.find({}).populate('event');
+    public async getAllMeetings(meetingParameters: MeetingParameter, loggedInUser: LoggedInUser): Promise<PagedResponse<Array<MeetingResponse>>> {
+        let meetingsQuery = this._repository.Meeting.find({user: loggedInUser.id});
+
+        if(meetingParameters.search) {
+            meetingsQuery.where({
+                $or: [
+                    {description: {$regex: meetingParameters.search, $options: 'i'}},
+                    {summary: {$regex: meetingParameters.search, $options: 'i'}},
+                ]
+            })
+        }
+
+        if (meetingParameters.type && meetingParameters.type.length > 0) {
+            meetingsQuery.where({
+                type: {
+                    $in : meetingParameters.type
+                }
+            })
+        }
+
+        if (meetingParameters.minStart && meetingParameters.maxEnd) {
+            meetingsQuery.where({
+                start: {
+                    $gte: meetingParameters.minStart.setSeconds(0,0)
+                },
+                end: {
+                    $lte: meetingParameters.maxEnd.setSeconds(0,0)
+                }
+            })
+        }
+
+        if (meetingParameters.inviteeEmail && meetingParameters.inviteeEmail.length > 0) {
+            meetingsQuery.where({
+                'invitee.email': {
+                    $in: meetingParameters.inviteeEmail
+                }
+            })
+        }
+        
+        if(meetingParameters.sort) {
+            meetingsQuery.sort(meetingParameters.sort)
+        } else {
+            meetingsQuery.sort('-createdAt')
+        }
+
+        const count = await meetingsQuery.clone().countDocuments();
+        const meetings = await meetingsQuery.skip(meetingParameters.pageSize * (meetingParameters.pageNumber - 1))
+        .limit(meetingParameters.pageSize)
+
+        const totalPages = Math.ceil(count / meetingParameters.pageSize);
+        const nextPage = meetingParameters.pageNumber < totalPages ? meetingParameters.pageNumber + 1: null
+        const previousPage = (meetingParameters.pageNumber > 1 && meetingParameters.pageNumber <= totalPages) 
+        ? meetingParameters.pageNumber - 1: null
+        const meta = new Meta(nextPage, previousPage, totalPages, meetingParameters.pageSize, count);
 
         const meetingsResponseDto = mapper.mapArray(meetings, Meeting, MeetingResponse);
 
-        return new SuccessResponse<Array<MeetingResponse>>(200, 'Meetings returned succesfully', meetingsResponseDto)
+        return new PagedResponse<Array<MeetingResponse>>(StatusCodes.OK, 'Meetings returned succesfully', meetingsResponseDto, meta)
     }
 }
